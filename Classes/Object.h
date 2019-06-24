911d9f09d099ff7884a1daadf5a6d8e5ba561032
Nguyen Tich Duy <nguyentichduy10a15cmg@gmailc.com>
2019-06-24 22:21:10 +0700

    +Finish and add funtion init for class Appdelegate

--- Classes/Object.h
+++ Classes/Object.h
@@ -1,1 +1,23 @@
 #pragma once
+#include"cocos2d.h"
+
+USING_NS_CC;
+
+class Objectt
+{
+public:
+	virtual void Update() = 0;
+	virtual void Init() = 0;
+	Objectt();
+	~Objectt();
+protected:
+	Sprite *m_sprite;
+};
+Objectt::Objectt()
+{
+
+}
+Objectt::~Objectt()
+{
+}
+
